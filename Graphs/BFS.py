32fcb1ea94e225025ce05db95554ab0959d87538
vikrant1998 <vikrant1998@Vikrant.localdomain>
2018-12-19 22:43:21 -0500

    updated bfs

--- Graphs/BFS.py
+++ Graphs/BFS.py
@@ -4,22 +4,33 @@ from collections import deque
 class Graph(object):
 	def __init__(self):
 		self.graph = defaultdict(list)
-		
+
 	def AddEdge(self, u, v):
-		self.graph[u].append(v)
+		if v not in self.graph[u]:
+			self.graph[u].append(v)
 
 	def BFS(self, u):
+		if u not in self.graph.keys(): return
+
 		visited, queue = set([u]), deque([u])
-		while queue: 
-			vertex = queue.popleft()
-			print (vertex)
-			for neighbor in self.graph[vertex]: 
-				if neighbor not in visited: 
-					visited.add(neighbor) 
-					queue.append(neighbor) 
-		
+
+		while queue:
+			element = queue.popleft()
+			print (element)
+
+			for neighbor in self.graph[element]:
+				if neighbor not in visited:
+					queue.append(neighbor)
+					visited.add(neighbor)
+
 if __name__ == "__main__":
 	g = Graph()
-	g.AddEdge(0, 1)
-	#g.AddEdge(1, 0)
+	g.AddEdge(1, 2)
+	g.AddEdge(1, 3)
+	g.AddEdge(2, 4)
+	g.AddEdge(2, 5)
+	g.AddEdge(5, 6)
+	g.AddEdge(5, 7)
+
+	print(g.graph)
 	g.BFS(1)
