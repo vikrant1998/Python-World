60ab61793f882bc2445bb2a2bfbdfbd6dad5ddf7
Vikrant Satheesh Kumar <vikrant1998@gmail.com>
2019-01-10 14:50:58 -0500

    .

--- Heap/KthLargestStream.py
+++ Heap/KthLargestStream.py
@@ -5,16 +5,28 @@ class KthLargest(object):
         :type k: int
         :type nums: List[int]
         """
+        self.kVal = k
+        self.heapList = []
+        for num in nums:
+            self.add(num)
 
     def add(self, val):
         """
         :type val: int
         :rtype: int
         """
+        import heapq
+        if len(self.heapList) < self.kVal:
+            heapq.heappush(self.heapList, (val))
+        elif val > self.heapList[0]:
+            heapq.heappop(self.heapList)
+            heapq.heappush(self.heapList, val)
+
+        return self.heapList[0]
 
 if __name__ == "__main__":
     # Your KthLargest object will be instantiated and called as such:
-    k = 3
+    k = 1
     nums = [4,5,8,2]
     obj = KthLargest(k, nums)
     print(obj.add(3))
