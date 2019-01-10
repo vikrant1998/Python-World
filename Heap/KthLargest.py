b512d9ee90cdbd802f486a1406ed316b7788def2
Vikrant Satheesh Kumar <vikrant1998@gmail.com>
2019-01-09 23:28:59 -0500

    .

---
+++ Heap/KthLargest.py
@@ -0,0 +1,20 @@
+class KthLargest(object):
+
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+
+if __name__ == "__main__":
+    # Your KthLargest object will be instantiated and called as such:
+    k = 3
+    nums = [4,5,8,2]
+    obj = KthLargest(k, nums)
+    print(obj.add(3))
