52f9bf2e249d0920cce980f3a0aa937714c62de9
Vikrant <vikrant1998@gmail.com>
2019-01-16 16:42:11 -0500

    .

---
+++ Graph/CourseSchedule.py
@@ -0,0 +1,62 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        self.graph = dict()
+        self.inDegreeSet = set()
+        self._BuildGraph(numCourses, prerequisites)
+        newSet = self._InDegreeCheck()
+
+        toposortList = []
+
+        from collections import deque
+        q = deque()
+
+        for key, value in self.graph.items():
+            if key not in newSet:
+                q.append(key)
+        
+        while q:
+            element = q.pop()
+            toposortList.append(element)
+
+            # Neighbors.
+            for neighbor in self.graph[element]:
+                import copy
+                currSet = copy.deepcopy(self.graph[element])
+                currSet.remove(neighbor)
+                self.graph[element] = currSet
+
+                retSet = self._InDegreeCheck()
+                if neighbor not in retSet:
+                    q.append(neighbor)
+
+        for key, value in self.graph.items():
+            if len(value) > 0:
+                return False
+
+        return True
+
+    def _BuildGraph(self, numCourses, prerequisites):
+        for i in range(0, numCourses):
+            self.graph[i] = set()
+
+        for connections in prerequisites:
+            self.graph[connections[1]].add(connections[0])
+
+    def _InDegreeCheck(self):
+        newSet = set()
+        for key, value in self.graph.items():
+            for element in value:
+                newSet.add(element)
+        return newSet
+
+if __name__ == "__main__":
+    s = Solution()
+    numCourses = 2
+    prerequisites = [[1,0],[0,1]]
+    print(s.canFinish(numCourses, prerequisites))
+        
