aed69de90ee6f92678c11d864b92cdba025cf3ac
Vikrant <vikrant1998@gmail.com>
2019-01-19 16:39:21 -0500

    .

---
+++ DP/StoneGame.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        return True
+
+if __name__ == "__main__":
+    piles = [5,3,4,5]
+    s = Solution()
+    s.stoneGame(piles)
