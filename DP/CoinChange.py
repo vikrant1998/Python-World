5e490e7d974d20d837fa03ced2b2a0fd9dbd2827
Vikrant <vikrant1998@gmail.com>
2019-01-29 18:37:39 -0500

    .

--- DP/CoinChange.py
+++ DP/CoinChange.py
@@ -1,3 +1,5 @@
+''' Recursion Timeout '''
+'''
 class Solution(object):
     def coinChange(self, coinsList, amount):
         """
@@ -26,9 +28,62 @@ class Solution(object):
             self.numCoins += 1
             self._recurse(i, amount - self.coins[i])
             self.numCoins -= 1
+'''
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [[0 for i in range(amount + 1)] for j in range(len(coins))]
+
+        if amount == 0: return 0
+
+        i = 0
+        j = 0
+
+        while i < len(dp):
+            j = 0
+            while j < len(dp[0]):
+
+                # Nothing on top.
+                if i - 1 < 0:
+                    if j % coins[i] == 0:
+                        dp[i][j] = j / coins[i]
+
+                # Stuff on top.
+                if i - 1 >= 0:
+                    if j < coins[i]:
+                        dp[i][j] = dp[i-1][j]
+                    elif dp[i-1][j] == 0 and dp[i][j-coins[i]] == 0:
+                        if j % coins[i] == 0:
+                            dp[i][j] = j / coins[i]
+                    elif dp[i][j-coins[i]] == 0:
+                        dp[i][j] = dp[i-1][j]
+                        if j % coins[i] == 0:
+                            dp[i][j] = min(dp[i][j], j / coins[i])
+                    elif dp[i-1][j] == 0:
+                        dp[i][j] = 1 + dp[i][j-coins[i]]
+                        if j % coins[i] == 0:
+                            dp[i][j] = min(dp[i][j], j / coins[i])
+                    else:
+                        dp[i][j] = min(dp[i-1][j], 1 + dp[i][j-coins[i]])
+                        if j % coins[i] == 0:
+                            dp[i][j] = min(dp[i][j], j / coins[i])
+
+                j += 1
+            i += 1
+
+        #print(dp)
+        if dp[-1][-1] == 0: return -1
+        return dp[-1][-1]
 
 if __name__ == "__main__":
     s = Solution()
-    coins = [2]
-    amount = 3
+    coins = [186,419,83,408]
+    #coins = [1, 2]
+    amount = 6249
+    #amount = 2
     print(s.coinChange(coins, amount))
