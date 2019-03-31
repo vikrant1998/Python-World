e90077c6ad7ddc02d8b2eb559720ec8e8961ef96
Vikrant <vikrant1998@gmail.com>
2019-03-31 19:40:32 -0400

    .

--- DP/UniquePaths.py
+++ DP/UniquePaths.py
@@ -14,8 +14,7 @@ class Solution(object):
         if (i, j) in self.cache: return self.cache[(i, j)]
         if i >= len(self.grid) or j >= len(self.grid[0]): return 0
         if i == len(self.grid) - 1 and j == len(self.grid[0]) - 1: return 1
-        numPaths = self._dfs(i + 1, j)
-        numPaths += self._dfs(i, j + 1)
+        numPaths = self._dfs(i + 1, j) + self._dfs(i, j + 1)
         self.cache[(i, j)] = numPaths
         return self.cache[(i, j)]
 
