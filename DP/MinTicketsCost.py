27eb941c0e18740986cb95f4f8275e0c39e34223
Vikrant <vikrant1998@gmail.com>
2019-04-19 15:02:13 -0400

    Push new changes

---
+++ DP/MinTicketsCost.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def mincostTickets(self, _days, _costs):
+        """
+        :type days: List[int]
+        :type costs: List[int]
+        :rtype: int
+        """
+        pass
+
+if __name__ == "__main__":
+    s = Solution()
+    days = [1,2,3,4,5,6,7,8,9,10,30,31]
+    costs = [2,7,15]
+    s.mincostTickets(days, costs)
